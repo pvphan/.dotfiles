b3f965e56bb74f3bec5d0ff8691688612c4e8a03
Vinh Phan <pvphan90@gmail.com>
2022-03-31 14:55:08 -0700

    installing mpv as well

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 for d in */ ; do
     stow --restow "$d"
 done
+
+mkdir -p ~/.config/mpv/
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ln -sf $SCRIPT_DIR/mpv/input.conf ~/.config/mpv/input.conf
