c735d79bc6c8c7348b9592d97681d5a515699124
Vinh Phan <pvphan90@gmail.com>
2022-12-08 22:55:24 -0800

    added pyright install

--- nvim/installplugins.sh
+++ nvim/installplugins.sh
@@ -2,3 +2,4 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt -y install nodejs
+sudo npm i -g pyright
