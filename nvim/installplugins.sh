0c3e58e81f5c2d098e9e89887102378a5aad4a0b
Vinh Phan <pvphan90@gmail.com>
2022-12-20 00:01:28 -0800

    tryin kickstart nvim

--- nvim/installplugins.sh
+++
@@ -1,5 +0,0 @@
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-sudo apt -y install nodejs
-sudo npm i -g pyright
