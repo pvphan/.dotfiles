ba9f7f4a2769b8a9583fa487175f49e4d4435286
Vinh Phan <pvphan90@gmail.com>
2022-12-20 00:38:03 -0800

    added

--- nvim/.config/nvim/lua/pvphan/remap.lua
+++ nvim/.config/nvim/lua/pvphan/remap.lua
@@ -13,3 +13,5 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
