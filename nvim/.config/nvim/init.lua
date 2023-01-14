bd9132c6723ea734e37f894344354af55ca227ad
Vinh Phan <pvphan90@gmail.com>
2023-01-13 17:48:20 -0800

    some fixes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -47,6 +47,9 @@ require('packer').startup(function(use)
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
 
+  -- Other
+  use 'godlygeek/tabular' -- Detect tabstop and shiftwidth automatically
+
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
