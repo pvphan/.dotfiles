bbceb6b51eb3e9d0a143dc49c451e4790f454f55
Vinh Phan <pvphan90@gmail.com>
2023-02-16 21:49:39 -0800

    removed block movements

--- nvim/.config/nvim/lua/pvphan/remap.lua
+++ nvim/.config/nvim/lua/pvphan/remap.lua
@@ -11,8 +11,5 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<return>", ":noh<return>")
