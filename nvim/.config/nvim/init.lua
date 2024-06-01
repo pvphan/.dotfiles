4308701c0726e61c95d1543c03ac1f82715c3bc4
Vinh Phan <pvphan90@gmail.com>
2024-06-01 14:10:29 -0700

    minor updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -207,10 +207,10 @@ require('Comment').setup()
 
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
-}
+-- require('indent_blankline').setup {
+--   char = '┊',
+--   show_trailing_blankline_indent = false,
+-- }
 
 -- Gitsigns
 -- See `:help gitsigns.txt`
--- nvim/.config/nvim/lua/pvphan/remap.lua
+++ nvim/.config/nvim/lua/pvphan/remap.lua
@@ -13,5 +13,4 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<return>", ":noh<return>")
